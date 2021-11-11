ed71c6556810902482566d7c94cb1b6d54a648fc
fmassola <fmassola@certant.com>
2021-11-11 12:53:33 -0300

    add database configurations

---
+++ Backend/src/database/connection.js
@@ -0,0 +1,26 @@
+const sql = require("mssql");
+
+//define db connection settings
+const dbSettings = {
+  user: "ensolvers",
+  password: "ensolvers123",
+  server: "localhost",
+  database: "testchallenge",
+  options: {
+    encrypt: true, // for azure
+    trustServerCertificate: true, // change to true for local dev / self-signed certs
+  },
+};
+
+//connect -> async function
+const getConnection = async () => {
+  //db request connection handler
+  try {
+    const pool = await sql.connect(dbSettings);
+    return pool;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getConnection();
--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,6 +1,7 @@
 //require app module
 const app = require("../src/app");
 
+//set port
 app.listen(app.get("port"));
 
 console.log("Server on port", app.get("port"));
