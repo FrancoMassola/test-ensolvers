d505a2f206a66450dc71a73103b39e1ed4850b48
fmassola <fmassola@certant.com>
2021-11-11 22:08:58 -0300

    add the first structure to the angular project

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FoldersViewComponent } from './pages/folders-view/folders-view.component';
+import { TasksViewComponent } from './pages/tasks-view/tasks-view.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'tasks', component: TasksViewComponent },
+  { path: 'folders', component:  FoldersViewComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TaskTableComponent } from './components/task-table/task-table.component';
+import { FolderTableComponent } from './components/folder-table/folder-table.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TasksViewComponent } from './pages/tasks-view/tasks-view.component';
+import { FoldersViewComponent } from './pages/folders-view/folders-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskTableComponent,
+    FolderTableComponent,
+    TasksViewComponent,
+    FoldersViewComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
