159b5e6b4ecbf081a6fc5bd0a4ffd5b47c51cae6
fmassola <fmassola@certant.com>
2021-11-12 01:00:19 -0300

    add a update page, one interface for tasks, a shared input component and a header

---
+++ Frontend/src/app/components/add-input-shared/add-input-shared.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-input-shared',
+  templateUrl: './add-input-shared.component.html',
+  styleUrls: ['./add-input-shared.component.css']
+})
+export class AddInputSharedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
