e4194c25bd1b4000a8652a198f5b3b2d79e67db5
fmassola <fmassola@certant.com>
2021-11-19 02:17:16 -0300

    update the start.sh settings

--- start.sh
+++ start.sh
@@ -1,23 +1,16 @@
 cd Backend 
-touch .env 
-echo 
-PORT=4000
-DB_USER = administrador
-DB_PASSWORD = admiN123
-DB_SERVER = testchallenge
-DB_DATABASE = testchallenge
- > .env
+echo -e " PORT=4000 \n DB_USER = ensolvers \n DB_PASSWORD = ensolvers123 \n DB_SERVER = localhost \n DB_DATABASE = testchallenge" > .env
 cd ..
 sudo systemctl Start mssql-server
 sqlcmd -S localhost -U ensolvers -P ensolvers123
-sqlcmd -q "CREATE TABLE Folder (
+sqlcmd -q "CREATE TABLE Folders (
     id_folder int IDENTITY(1,1) PRIMARY KEY,
     name_folder varchar(40) NOT NULL
 );"  
-sqlcmd -q "CREATE TABLE Task (
+sqlcmd -q "CREATE TABLE Tasks (
     id_task int IDENTITY(1,1) PRIMARY KEY,
     name_task varchar(40) NOT NULL,
 	status_task bit,
-    id_task_folder int FOREIGN KEY REFERENCES Folder(id_folder)ON DELETE CASCADE 
+    id_task_folder int FOREIGN KEY REFERENCES Folders(id_folder)ON DELETE CASCADE 
 );"
 docker-compose -f composer.yaml up
